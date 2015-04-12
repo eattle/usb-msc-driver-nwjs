d24bd83bb1c970b5020e7bd8887157e20c0fb663
류형욱 <mintrupt@mintrupt.com>
2015-04-12 15:38:47 +0900

    Eattle 헤더 검증

--- test/format.js
+++ test/format.js
@@ -126,4 +126,62 @@ describe('format', function () {
       });
     });
   });
+  describe('Eattle', function () {
+    const MemoryBlockDevice = riccardo.get('memoryBlockDevice');
+    var device = new MemoryBlockDevice({ size: 1000000000 });
+    it('주어진 오프셋을 적용하여 에외 또는 오류 없이 포맷에 성공해야 합니다.', function () {
+      format.formatEattle(device, 4200, function (err) {
+        assert.strictEqual(err, null);
+        assert.strictEqual(Array.prototype.slice.call(arguments).length, 1);
+      });
+    });
+    it('시그니처가 지정되어야 합니다.', function () {
+      var buffer = new Buffer(512);
+      device.readBlock(4200, buffer, function (err, result) {
+        assert.strictEqual(err, null);
+        assert.strictEqual(result[0], 0xa3);
+        assert.strictEqual(result[1], 0x75);
+        assert.strictEqual(result[2], 0x80);
+        assert.strictEqual(result[3], 0xc5);
+        assert.strictEqual(result[4], 0x44);
+        assert.strictEqual(result[5], 0x28);
+        assert.strictEqual(result[6], 0x44);
+        assert.strictEqual(result[7], 0x71);
+        assert.strictEqual(result[8], 0x3e);
+        assert.strictEqual(result[9], 0xa1);
+        assert.strictEqual(result[10], 0x1d);
+        assert.strictEqual(result[11], 0xe5);
+        assert.strictEqual(result[12], 0xb2);
+        assert.strictEqual(result[13], 0xee);
+        assert.strictEqual(result[14], 0x50);
+        assert.strictEqual(result[15], 0x29);
+        assert.strictEqual(result[16], 0x69);
+        assert.strictEqual(result[17], 0x9d);
+        assert.strictEqual(result[18], 0x22);
+        assert.strictEqual(result[19], 0x77);
+        assert.strictEqual(result[20], 0x7d);
+        assert.strictEqual(result[21], 0x63);
+        assert.strictEqual(result[22], 0x61);
+        assert.strictEqual(result[23], 0x8c);
+        assert.strictEqual(result[24], 0x06);
+        assert.strictEqual(result[25], 0xec);
+        assert.strictEqual(result[26], 0x52);
+        assert.strictEqual(result[27], 0x72);
+        assert.strictEqual(result[28], 0xa6);
+        assert.strictEqual(result[29], 0x51);
+        assert.strictEqual(result[30], 0x57);
+        assert.strictEqual(result[31], 0x27);
+      });
+    });
+    it('지금은 예약된 블럭 수가 0이어야 합니다.', function () {
+      var buffer = new Buffer(512);
+      device.readBlock(4200, buffer, function (err, result) {
+        assert.strictEqual(err, null);
+        assert.strictEqual(result[508], 0x00);
+        assert.strictEqual(result[509], 0x00);
+        assert.strictEqual(result[510], 0x00);
+        assert.strictEqual(result[511], 0x00);
+      });
+    });
+  });
 });
