71f98ceded885de7f0ba7b8fe7de8fd71d41a7d6
류형욱 <mintrupt@mintrupt.com>
2015-04-12 15:30:16 +0900

    모듈 이름 변경

--- test/memoryBlockDevice.js
+++ test/memoryBlockDevice.js
@@ -4,34 +4,34 @@ const Riccardo = require('riccardo');
 var riccardo = new Riccardo();
 riccardo.scan('../lib', '');
 
-describe('memoryDriver', function () {
+describe('memoryBlockDevice', function () {
 
-  const MemoryDriver = riccardo.get('memoryDriver');
+  const MemoryBlockDevice = riccardo.get('memoryBlockDevice');
 
   it('컴포넌트가 있어야 합니다.', function () {
-    assert(MemoryDriver);
+    assert(MemoryBlockDevice);
   });
 
   it('설정이 없으면 예외를 던져야 합니다.', function () {
     assert.throws(function () {
-      new MemoryDriver();
+      new MemoryBlockDevice();
     });
   });
   
   describe('크기 10', function () {    
-    var memoryDriver = null;
+    var device = null;
     it('초기화 성공해야 합니다.', function () {
       var config = {
         size: 10
       };
-      memoryDriver = new MemoryDriver(config);
+      device = new MemoryBlockDevice(config);
     });
 
-    it('getLastLogicalBlockAddress가 10을 반환해야 합니다.', function () {
-      assert.strictEqual(memoryDriver.getLastLogicalBlockAddress(), 10);
+    it('getLastLogicalBlockAddress가 9를 반환해야 합니다.', function () {
+      assert.strictEqual(device.getLastLogicalBlockAddress(), 9);
     });
     it('getBlockLength가 512를 반환해야 합니다.', function () {
-      assert.strictEqual(memoryDriver.getBlockLength(), 512);
+      assert.strictEqual(device.getBlockLength(), 512);
     });
 
     describe('readBlock', function () {
@@ -44,9 +44,9 @@ describe('memoryDriver', function () {
             done();
           }
         };
-        for (var i = 1; i <= 10; i++) {
+        for (var i = 0; i <= 9; i++) {
           var buffer = new Buffer(512);
-          memoryDriver.readBlock(i, buffer, callback);
+          device.readBlock(i, buffer, callback);
         }
       });
       it('처음에 모든 블럭의 값이 0이어야 합니다.', function (done) {
@@ -60,40 +60,40 @@ describe('memoryDriver', function () {
             done();
           }
         };
-        for (var i = 1; i <= 10; i++) {
+        for (var i = 0; i <= 9; i++) {
           var buffer = new Buffer(512);
-          memoryDriver.readBlock(i, buffer, callback);
+          device.readBlock(i, buffer, callback);
         }
       });
       it('읽을 때 범위를 벗어나면 예외를 던져야 합니다.', function (done) {
         assert.throws(function () {
           var buffer = new Buffer(512);
-          memoryDriver.readBlock(-1, buffer, done);
+          device.readBlock(-1, buffer, done);
         });
         assert.throws(function () {
           var buffer = new Buffer(512);
-          memoryDriver.readBlock(11, buffer, done);
+          device.readBlock(10, buffer, done);
         });
         assert.throws(function () {
           var buffer = new Buffer(512);
-          memoryDriver.readBlock(42, buffer, done);
+          device.readBlock(42, buffer, done);
         });
         done();
       });
       it('읽을 때 buffer가 없으면 예외를 던져야 합니다.', function (done) {
         assert.throws(function () {
-          memoryDriver.readBlock(5, null, done);
+          device.readBlock(5, null, done);
         });
         done();
       });
       it('읽을 때 버퍼 크기가 512가 아니면 예외를 던져야 합니다.', function (done) {
         assert.throws(function () {
           var buffer = new Buffer(511);
-          memoryDriver.readBlock(5, buffer, done);
+          device.readBlock(5, buffer, done);
         });
         assert.throws(function () {
           var buffer = new Buffer(513);
-          memoryDriver.readBlock(5, buffer, done);
+          device.readBlock(5, buffer, done);
         });
         done();
       });
@@ -107,41 +107,41 @@ describe('memoryDriver', function () {
             done();
           }
         };
-        for (var i = 1; i <= 10; i++) {
+        for (var i = 0; i <= 9; i++) {
           var buffer = new Buffer(512);
           buffer.fill(i);
-          memoryDriver.readBlock(i, buffer, callback);
+          device.readBlock(i, buffer, callback);
         }
       });
       it('쓸 때 범위를 벗어나면 예외를 던져야 합니다.', function (done) {
         assert.throws(function () {
           var buffer = new Buffer(512);
-          memoryDriver.writeBlock(-1, buffer, done);
+          device.writeBlock(-1, buffer, done);
         });
         assert.throws(function () {
           var buffer = new Buffer(512);
-          memoryDriver.writeBlock(11, buffer, done);
+          device.writeBlock(11, buffer, done);
         });
         assert.throws(function () {
           var buffer = new Buffer(512);
-          memoryDriver.writeBlock(42, buffer, done);
+          device.writeBlock(42, buffer, done);
         });
         done();
       });
       it('쓸 때 버퍼가 없으면 예외를 던져야 합니다.', function (done) {
         assert.throws(function () {
-          memoryDriver.writeBlock(5, null, done);
+          device.writeBlock(5, null, done);
         });
         done();
       });
       it('쓸 때 버퍼 크기가 512가 아니면 예외를 던져야 합니다.', function (done) {
         assert.throws(function () {
           var buffer = new Buffer(511);
-          memoryDriver.writeBlock(5, buffer, done);
+          device.writeBlock(5, buffer, done);
         });
         assert.throws(function () {
           var buffer = new Buffer(513);
-          memoryDriver.writeBlock(5, buffer, done);
+          device.writeBlock(5, buffer, done);
         });
         done();
       });
@@ -153,10 +153,10 @@ describe('memoryDriver', function () {
             done();
           }
         };
-        for (var i = 1; i <= 10; i++) {
+        for (var i = 0; i <= 9; i++) {
           var buffer = new Buffer(512);
           buffer.fill(i);
-          memoryDriver.writeBlock(i, buffer, callback);
+          device.writeBlock(i, buffer, callback);
         }
       });
     });
@@ -174,23 +174,23 @@ describe('memoryDriver', function () {
             }
           };
         }; 
-        for (var i = 1; i <= 10; i++) {
+        for (var i = 0; i <= 9; i++) {
           var buffer = new Buffer(512);
-          memoryDriver.readBlock(i, buffer, callback(i));
+          device.readBlock(i, buffer, callback(i));
         }
       });
     });
   });
   describe('크기 1000000000, 게으른 할당 시험', function () {
-    var memoryDriver = null;
+    var device = null;
     it('초기화 성공해야 합니다.', function () {
       var config = {
         size: 1000000000
       };
-      memoryDriver = new MemoryDriver(config);
+      device = new MemoryBlockDevice(config);
     });
     it('getLastLogicalBlockAddress가 1000000000을 반환해야 합니다.', function () {
-      assert.strictEqual(memoryDriver.getLastLogicalBlockAddress(), 1000000000);
+      assert.strictEqual(device.getLastLogicalBlockAddress(), 999999999);
     });
     it('임의 번지에 쓰기, 읽기 작업을 100회 반복합니다.', function (done) {
       var remaining = 100;
@@ -211,11 +211,11 @@ describe('memoryDriver', function () {
         for (var i = 0; i < 512; i++) {
           bufferExpect[i] = rand() % 256;
         }
-        memoryDriver.writeBlock(lba, bufferExpect, function (err, bufferWrite) {
+        device.writeBlock(lba, bufferExpect, function (err, bufferWrite) {
           assert.strictEqual(err, null);
           assert.strictEqual(bufferExpect, bufferWrite);
           var bufferActual = new Buffer(512);
-          memoryDriver.readBlock(lba, bufferActual, function (err, result) {
+          device.readBlock(lba, bufferActual, function (err, result) {
             assert.strictEqual(err, null);
             assert.strictEqual(bufferActual, result);
             assert.notStrictEqual(bufferExpect, bufferActual);
